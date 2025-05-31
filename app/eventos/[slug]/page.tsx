96a690f405de7adc7cb5d307606e16c548b89cc3
James-Guevara-Gamboa <jjguevarag@gmail.com>
2025-05-31 17:57:30 -0600

    feat: update EventDetailPage to use React's use hook for improved slug handling

--- app/eventos/[slug]/page.tsx
+++ app/eventos/[slug]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { EventsDetailTemplate } from "@/components/templates/events-detail-template";
+import { use } from "react";
 
-export default function EventDetailPage({ params }: { params: { slug: string } }) {
-  return <EventsDetailTemplate slug={params.slug} />;
+export default function EventDetailPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = use(params);
+  return <EventsDetailTemplate slug={slug} />;
 }
